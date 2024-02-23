37c5a88f1536a12c58129579489de7ba99a29b19
KwabenaArhinkorah <132001163+KwabenaArhinkorah@users.noreply.github.com>
2024-02-23 03:25:17 +0000

    Add files via upload

---
+++ _message_script.js
@@ -0,0 +1,16 @@
+const conversations = [
+    {
+        id: 1,
+        recipient: "John Snow",
+        lastMessage: "Hi! How are you?",
+        timestamp: "2024-02-22T14:20:00Z"
+    },
+    {
+        id: 2,
+        recipient: "Jane Mary",
+        lastMessage:"when are you leaving for the event"
+    }
+
+    
+]
+
